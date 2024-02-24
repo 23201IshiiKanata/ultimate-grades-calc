78d0232ae5daa1eb9ad140f04bb4c84dbe22046f
23236ShimadaSota <158822191+23236ShimadaSota@users.noreply.github.com>
2024-02-24 18:03:18 +0900

    sa-ba-sisutemu

---
+++ server/host.js
@@ -0,0 +1,69 @@
+; (async () => {
+  console.log('host.js', 'ready');
+  // シグナリングサーバーであるWebSocketサーバーに接続
+  // 今回はsocket.ioを採用
+  const socket = ('http://localhost:55555');
+
+  // require('socket.io-client')
+
+  /**
+   * RTCPeerConnectionをクライアントごとに格納する変数
+   * keyをクライアントID（ソケットID）として保存する
+   */
+  const connections = {};
+  console.log(connections);
+
+  /**
+   * PC映像streamを取得
+   * @type {MediaStream}
+   */
+
+  // ソケットサーバー疎通確認
+  socket.on('connect', () => console.log('socket', 'connected'));
+
+  // 配信要求を受ける
+  // Client ID （cid）を受け取りコネクションを作成する
+  socket.on('request', ({cid}) => sendOffer(cid));
+
+  // アンサーを受ける
+  socket.on('answer', async ({cid, answer}) => {
+    if (cid in connections) connections[cid].setRemoteDescription(answer);
+  });
+
+  /**
+   * オファーを送信する
+   *
+   * @param {string} cid Client ID
+   * @return {void}
+   */
+  async function sendOffer(cid) {
+    // コネクションの設定
+    const pcConfig = {
+      // STUNサーバーはGoogle様のものを利用させていただく
+      iceServers: [{urls: 'stun:stun.l.google.com:19302'}],
+    };
+
+    // コネクションの作成
+    const peer = new RTCPeerConnection(pcConfig);
+
+    // cidをキーとしてコネクションを保存
+    connections[cid] = peer;
+
+    // コネクションにストリームを設定
+    stream.getTracks().forEach(track => peer.addTrack(track, stream));
+
+    // ICE candidateを取得イベントハンドラ
+    peer.onicecandidate = evt => {
+      // evt.candidateがnullならICE Candidateを全て取得したとみなしてオファーを送信
+      if (!evt.candidate)
+        socket.emit('offer', { offer: peer.localDescription, cid });
+    };
+
+    // オファーを作成
+    const offer = await peer.createOffer();
+
+    // オファーを自身に設定
+    // STUNサーバーへアクセスが始まり、onicecandidateが呼ばれるようになる
+    await peer.setLocalDescription(offer);
+  }
+})();
--- server/index.js
+++ server/index.js
@@ -2,39 +2,41 @@
  * シグナリングサーバー（WebSocketサーバー） + Webサーバー
  */
 // Express
-const express = require('express')
-const app = express()
+console.log('main', 'start');
+
+const express = require('express');
+const app = express();
 
 // publicディレクトリを公開
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public'));
 
-const http = require('http')
-const server = http.createServer(app)
+const http = require('http');
+const server = http.createServer(app);
 
 // WebSocketサーバーにはsocket.ioを採用
-const io = require('socket.io')(server)
+const io = require('socket.io')(server);
 
 // 接続要求
 io.on('connect', socket => {
-  console.log('io', 'connect')
-  console.log('io', 'socket: ', socket.id)
+  console.log('io', 'connect');
+  console.log('io', 'socket: ', socket.id);
 
   // 受信側からの配信要求を配信側へ渡す
   socket.on('request', () =>
-    socket.broadcast.emit('request', { cid: socket.id })
-  )
+    socket.broadcast.emit('request', {cid: socket.id}),
+  );
 
   // 配信側からのオファーを受信側へ渡す
-  socket.on('offer', ({ offer }) => {
-    socket.broadcast.emit('offer', { offer })
+  socket.on('offer', ({offer}) => {
+    socket.broadcast.emit('offer', {offer});
     // 配信側の接続が切れた場合にそれを受信側へ通知する
-    socket.on('disconnect', () => socket.broadcast.emit('close'))
-  })
+    socket.on('disconnect', () => socket.broadcast.emit('close'));
+  });
 
   // 受信側からのアンサーを配信側へ渡す
-  socket.on('answer', ({ answer }) =>
-    socket.broadcast.emit('answer', { cid: socket.id, answer })
-  )
-})
+  socket.on('answer', ({answer}) =>
+    socket.broadcast.emit('answer', {cid: socket.id, answer}),
+  );
+});
 
-server.listen(55555)
+server.listen(55555);
