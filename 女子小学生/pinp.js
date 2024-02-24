df881061baad0674645f6f3fca6352a041d13e2f
23201IshiiKanata <158822563+23201IshiiKanata@users.noreply.github.com>
2024-02-24 16:53:56 +0900

    PinPの動きを改善

--- 女子小学生/pinp.js
+++ 女子小学生/pinp.js
@@ -2,77 +2,23 @@
  * 読み込み完了時に一度だけ実行される。
  */
 $(() => {
-  // 要素の取得
-  var elements = document.getElementsByClassName('drag-and-drop');
-
-  // 要素内のクリックされた位置を取得するグローバル（のような）変数
-  var x;
-  var y;
-
-  // マウスが要素内で押されたとき、又はタッチされたとき発火
-  for (var i = 0; i < elements.length; i++) {
-    elements[i].addEventListener('mousedown', mdown, false);
-    elements[i].addEventListener('touchstart', mdown, false);
-  }
-
-  // マウスが押された際の関数
-  function mdown(e) {
-    // クラス名に .drag を追加
-    this.classList.add('drag');
-
-    // タッチデイベントとマウスのイベントの差異を吸収
-    if (e.type === 'mousedown') {
-      var event = e;
-    } else {
-      var event = e.changedTouches[0];
-    }
-
-    // 要素内の相対座標を取得
-    x = event.pageX - this.offsetLeft;
-    y = event.pageY - this.offsetTop;
-
-    // ムーブイベントにコールバック
-    document.body.addEventListener('mousemove', mmove, false);
-    document.body.addEventListener('touchmove', mmove, false);
-  }
-
-  // マウスカーソルが動いたときに発火
-  function mmove(e) {
-    // ドラッグしている要素を取得
-    var drag = document.getElementsByClassName('drag')[0];
-
-    // 同様にマウスとタッチの差異を吸収
-    if (e.type === 'mousemove') {
-      var event = e;
-    } else {
-      var event = e.changedTouches[0];
-    }
-
-    // フリックしたときに画面を動かさないようにデフォルト動作を抑制
-    e.preventDefault();
-
-    // マウスが動いた場所に要素を動かす
-    drag.style.top = event.pageY - y + 'px';
-    drag.style.left = event.pageX - x + 'px';
-
-    // マウスボタンが離されたとき、またはカーソルが外れたとき発火
-    drag.addEventListener('mouseup', mup, false);
-    document.body.addEventListener('mouseleave', mup, false);
-    drag.addEventListener('touchend', mup, false);
-    document.body.addEventListener('touchleave', mup, false);
-  }
-
-  // マウスボタンが上がったら発火
-  function mup(e) {
-    var drag = document.getElementsByClassName('drag')[0];
-
-    // ムーブベントハンドラの消去
-    document.body.removeEventListener('mousemove', mmove, false);
-    drag.removeEventListener('mouseup', mup, false);
-    document.body.removeEventListener('touchmove', mmove, false);
-    drag.removeEventListener('touchend', mup, false);
-
-    // クラス名 .drag も消す
-    drag.classList.remove('drag');
-  }
+  const element = $('.drag-and-drop')[0];
+  element.onpointerdown = (event) => {
+    element.onpointermove = (event) => {
+      const x = event.clientX - Number(element.offsetWidth) / 2;
+      const y = event.clientY - Number(element.offsetHeight) / 2;
+      const play = 10; // PinPの遊び
+      const maxX = window.innerWidth - Number(element.offsetWidth) - play;
+      const maxY = window.innerHeight - Number(element.offsetHeight) - play;
+      element.style.transform = `translate(
+        ${Math.max(Math.min(x, maxX), play)}px,
+        ${Math.max(Math.min(y, maxY), play)}px
+      )`;
+    };
+    element.setPointerCapture(event.pointerId);
+  };
+  element.onpointerup = (event) => {
+    element.onpointermove = null;
+    element.releasePointerCapture(event.pointerId);
+  };
 });
