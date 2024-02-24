3ee55069ac6db154b461375ed434f301a23818e8
23236ShimadaSota <158822191+23236ShimadaSota@users.noreply.github.com>
2024-02-24 17:11:14 +0900

    sa-ba-sofutohennkou

--- server/index.js
+++ server/index.js
@@ -1,16 +1,40 @@
-let server = require('ws').Server;
-let s = new server({port: 5001});
+/**
+ * シグナリングサーバー（WebSocketサーバー） + Webサーバー
+ */
+// Express
+const express = require('express')
+const app = express()
 
-s.on('connection', function(ws) {
-  ws.on('message', function(message) {
-    console.log('Received: ' + message);
+// publicディレクトリを公開
+app.use(express.static(__dirname + '/public'))
 
-    s.clients.forEach(function(client) {
-      client.send(message + ' : ' + new Date());
-    });
-  });
+const http = require('http')
+const server = http.createServer(app)
 
-  ws.on('close', function() {
-    console.log('I lost a client');
-  });
-});
+// WebSocketサーバーにはsocket.ioを採用
+const io = require('socket.io')(server)
+
+// 接続要求
+io.on('connect', socket => {
+  console.log('io', 'connect')
+  console.log('io', 'socket: ', socket.id)
+
+  // 受信側からの配信要求を配信側へ渡す
+  socket.on('request', () =>
+    socket.broadcast.emit('request', { cid: socket.id })
+  )
+
+  // 配信側からのオファーを受信側へ渡す
+  socket.on('offer', ({ offer }) => {
+    socket.broadcast.emit('offer', { offer })
+    // 配信側の接続が切れた場合にそれを受信側へ通知する
+    socket.on('disconnect', () => socket.broadcast.emit('close'))
+  })
+
+  // 受信側からのアンサーを配信側へ渡す
+  socket.on('answer', ({ answer }) =>
+    socket.broadcast.emit('answer', { cid: socket.id, answer })
+  )
+})
+
+server.listen(55555)
